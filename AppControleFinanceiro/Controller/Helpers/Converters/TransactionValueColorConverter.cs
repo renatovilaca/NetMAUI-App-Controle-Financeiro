6df9d378067eceb44533a69186762863d039b012
Renato Vilaça <renatovilaca@gmail.com>
2023-03-01 23:31:01 -0300

    Applied colors for transaction values

---
+++ AppControleFinanceiro/Controller/Helpers/Converters/TransactionValueColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using AppControleFinanceiro.Controller.Enums;
+using AppControleFinanceiro.Controller.Models;
+using System.Globalization;
+
+namespace AppControleFinanceiro.Controller.Helpers.Converters
+{
+    public class TransactionValueColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Transaction transaction = (Transaction)value;
+
+            if (transaction == null)  return null;
+
+            if (transaction.Type == TransactionType.Income)
+                return Color.FromArgb("#FF939E5A");
+            else
+                return Colors.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AppControleFinanceiro/Controller/Helpers/Converters/TransactionValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using AppControleFinanceiro.Controller.Enums;
+using AppControleFinanceiro.Controller.Models;
+using System.Globalization;
+
+namespace AppControleFinanceiro.Controller.Helpers.Converters
+{
+    public class TransactionValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Transaction transaction = (Transaction)value;
+
+            if (transaction == null) return null;
+
+            if (transaction.Type == TransactionType.Income)
+                return transaction.Value.ToString("C");
+            else
+                return $"-{transaction.Value.ToString("C")}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
